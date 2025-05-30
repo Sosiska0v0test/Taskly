5b69b76d9d5a7691cbb86b5c36490a2643dc2ccb
Sosiska0v0test <117019284+Sosiska0v0test@users.noreply.github.com>
2025-05-30 21:23:10 +0300

    Add files via upload

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { Search } from "lucide-react";
+import { useDispatch } from "react-redux";
+import { setSearchedQuery } from "@/redux/jobSlice";
+import { useNavigate } from "react-router-dom";
+
+const HeroSection = () => {
+  const [query, setQuery] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const searchJobHandler = () => {
+    dispatch(setSearchedQuery(query));
+    navigate("/browse");
+  };
+
+  return (
+    <div className="text-center">
+      <div className="flex flex-col gap-5 my-10">
+        <h1 className="text-5xl font-bold">
+          <span className="text-[#616161]">Let's search for a project</span>
+        </h1>
+
+        <div className="flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto">
+          <input
+            type="text"
+            placeholder="Project Title"
+            onChange={(e) => setQuery(e.target.value)}
+            className="outline-none border-none w-full"
+          />
+          <Button
+            onClick={searchJobHandler}
+            className="rounded-r-full bg-[#616161]">
+            <Search className="h-5 w-5" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ client/src/components/shared/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className="border-t border-t-gray-200 py-8">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="mb-4 md:mb-0">
+            <p className="text-sm">© 2025 Your Project. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
